9bbfd8e3f7f7716193e04c861d956de7522bf0a5
afhorton <106110920+afhorton@users.noreply.github.com>
2024-04-07 08:10:13 -0600

    Initial Commit

---
+++ main.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var side = 'p1'
+var p1_score = 0
+var p2_score = 0
+
---
+++ pong_game.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Label.text = str(Main.p1_score)
+	$Label2.text = str(Main.p2_score)
+	
+	
+
+func _on_top_body_entered(body):
+	body.direction.y *= -1
+
+
+func _on_bottom_body_entered(body):
+	body.direction.y *= -1
+
+
+func _on_left_body_entered(body):
+	body.queue_free()
+	var e = preload("res://ball.tscn").instantiate()
+	e.global_position = Vector2(570,320)
+	add_child(e)
+	Main.p2_score += 1
+
+
+func _on_right_body_entered(body):
+	body.queue_free()
+	var e = preload("res://ball.tscn").instantiate()
+	e.global_position = Vector2(570,320)
+	add_child(e)
+	Main.p1_score += 1
+
